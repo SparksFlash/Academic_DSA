ad98a6c408cf42f6d53adf0f3709193bf379cba6
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-10-10 23:49:31 +0600

    pop

--- Stack_pop.cpp
+++ Stack_pop.cpp
@@ -3,4 +3,17 @@ using namespace std;
 int main()
 {
     stack<int> st;
+    st.push(1);
+    st.push(2);
+    st.push(3);
+    st.push(4);
+
+    st.pop();
+    st.pop();
+
+    while (!st.empty())
+    {
+        cout << ' ' << st.top();
+        st.pop();
+    }
 }
