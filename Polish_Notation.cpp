91df89852868e90dc70d2c7e1944b67c459081ff
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-10-13 23:33:52 +0600

    2nd

--- Polish_Notation.cpp
+++ Polish_Notation.cpp
@@ -6,6 +6,17 @@ bool isOperator(char c)
     return (!isalpha(c) && !isdigit(c));
 }
 
+int getPriority(char c)
+{
+    if (c == '-' || c == '+')
+        return 1;
+    if (c == '*' || c == '/')
+        return 2;
+    if (c == '^')
+        return 3;
+    return 0;
+}
+
 int main()
 {
 }
